a0c21bf4f2179e09b9ef40be3b5a3693b9635b42
Mark Schmieder <markhschmieder@gmail.com>
2026-06-13 19:22:37 -0400

    update dependencies in POM files

--- src/main/java/com/mhschmieder/fxchart/input/CartesianCursorCoordinatesTracker.java
+++ src/main/java/com/mhschmieder/fxchart/input/CartesianCursorCoordinatesTracker.java
@@ -32,8 +32,8 @@ package com.mhschmieder.fxchart.input;
 
 import com.mhschmieder.fxgraphics.geometry.GeometryUtilities;
 import com.mhschmieder.fxgraphics.input.ClickLocation;
-import com.mhschmieder.jgraphics.input.MouseToolManager;
 import com.mhschmieder.fxgui.input.CursorCoordinatesTracker;
+import com.mhschmieder.jgraphics.input.MouseToolManager;
 import com.mhschmieder.jphysics.DistanceUnit;
 import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
