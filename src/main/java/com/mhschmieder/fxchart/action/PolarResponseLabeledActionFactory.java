b2d5027e3c1384e5d8f59483e84ed9c4b5b7fd0f
Mark Schmieder <markhschmieder@gmail.com>
2026-06-21 12:16:33 -0400

    Prepare plotting and charting code for next steps of finalization to 1.0.0 release with Java 25

---
+++ src/main/java/com/mhschmieder/fxchart/action/PolarResponseLabeledActionFactory.java
@@ -0,0 +1,115 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020, 2026 Mark Schmieder. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * This file is part of the FxPolarChart Library
+ *
+ * You should have received a copy of the MIT License along with the FxPolarChart
+ * Library. If not, see <https://opensource.org/licenses/MIT>.
+ *
+ * Project: https://github.com/mhschmieder/fxpolarchart
+ */
+package com.mhschmieder.fxchart.action;
+
+import com.mhschmieder.fxcontrols.action.ActionFactory;
+import com.mhschmieder.fxcontrols.action.LabeledActionFactory;
+import com.mhschmieder.fxcontrols.action.XAction;
+import com.mhschmieder.fxcontrols.action.XActionGroup;
+import com.mhschmieder.jcommons.util.ClientProperties;
+import org.controlsfx.control.action.Action;
+
+import java.util.Collection;
+
+/**
+ * This is a utility class for making labeled actions for Polar Response.
+ */
+public class PolarResponseLabeledActionFactory {
+
+    // NOTE: We must substitute "." for resource directory tree delimiters.
+    public static final String BUNDLE_NAME = "properties.PolarResponseActionLabels";
+
+    @SuppressWarnings("nls")
+    public static final XActionGroup getScaleChoiceGroup( final ClientProperties pClientProperties,
+                                                          final PolarResponseAmplitudeScaleChoices polarResponseAmplitudeScaleChoices ) {
+        final Collection< Action > scaleChoiceCollection = polarResponseAmplitudeScaleChoices.getScaleChoiceCollection();
+
+        final XActionGroup scaleChoiceGroup = ActionFactory.makeChoiceGroup( pClientProperties,
+                                                                             scaleChoiceCollection,
+                                                                             BUNDLE_NAME,
+                                                                             "scale",
+                                                                             null );
+
+        return scaleChoiceGroup;
+    }
+
+    @SuppressWarnings("nls")
+    public static final XAction getScaleDiv10dbChoice( final ClientProperties pClientProperties ) {
+        return getScaleDivChoice( pClientProperties, "div10db" );
+    }
+
+    @SuppressWarnings("nls")
+    public static final XAction getScaleDiv5dbChoice( final ClientProperties pClientProperties ) {
+        return getScaleDivChoice( pClientProperties, "div5db" );
+    }
+
+    @SuppressWarnings("nls")
+    public static final XAction getScaleDiv6dbChoice( final ClientProperties pClientProperties ) {
+        return getScaleDivChoice( pClientProperties, "div6db" );
+    }
+
+    @SuppressWarnings("nls")
+    private static final XAction getScaleDivChoice( final ClientProperties pClientProperties,
+                                                    final String itemName ) {
+        return ActionFactory.makeChoice( pClientProperties, BUNDLE_NAME, "scale", itemName, null );
+    }
+
+    @SuppressWarnings("nls")
+    public static final XActionGroup getTestActionGroup( final ClientProperties pClientProperties,
+                                                         final TestActions testActions ) {
+        final Collection< Action > testActionCollection = testActions.getTestActionCollection();
+
+        final XActionGroup testActionGroup = ActionFactory
+                .makeActionGroup( pClientProperties,
+                                  testActionCollection,
+                                  LabeledActionFactory.BUNDLE_NAME,
+                                  "test",
+                                  null );
+
+        return testActionGroup;
+    }
+
+    @SuppressWarnings("nls")
+    public static final XActionGroup getViewActionGroup( final ClientProperties pClientProperties,
+                                                         final PolarResponseViewActions polarResponseViewActions ) {
+        final Collection< Action > viewActionCollection = polarResponseViewActions
+                .getViewActionCollection( pClientProperties );
+
+        final XActionGroup viewActionGroup = ActionFactory
+                .makeActionGroup( pClientProperties,
+                                  viewActionCollection,
+                                  LabeledActionFactory.BUNDLE_NAME,
+                                  "view",
+                                  null );
+
+        return viewActionGroup;
+    }
+}
---
+++ src/main/java/com/mhschmieder/fxchart/control/PolarResponseMenuFactory.java
@@ -0,0 +1,95 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020, 2026 Mark Schmieder. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * This file is part of the FxPolarChart Library
+ *
+ * You should have received a copy of the MIT License along with the FxPolarChart
+ * Library. If not, see <https://opensource.org/licenses/MIT>.
+ *
+ * Project: https://github.com/mhschmieder/fxpolarchart
+ */
+package com.mhschmieder.fxchart.control;
+
+import com.mhschmieder.fxchart.action.TestActions;
+import com.mhschmieder.fxcontrols.action.XActionGroup;
+import com.mhschmieder.fxcontrols.action.XActionUtilities;
+import com.mhschmieder.fxchart.action.PolarResponseActions;
+import com.mhschmieder.fxchart.action.PolarResponseAmplitudeScaleChoices;
+import com.mhschmieder.fxchart.action.PolarResponseLabeledActionFactory;
+import com.mhschmieder.fxchart.action.PolarResponseViewActions;
+import com.mhschmieder.jcommons.util.ClientProperties;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import org.controlsfx.control.action.Action;
+
+import java.util.Collection;
+
+/**
+ * This is a factory class for generating Menus for Polar Response.
+ */
+public final class PolarResponseMenuFactory {
+
+    public static ContextMenu getPolarResponseContextMenu( final ClientProperties pClientProperties,
+                                                           final PolarResponseActions polarResponseActions ) {
+        final Collection< Action > polarResponseContextMenuActionCollection = polarResponseActions
+                .getPolarResponseContextMenuActionCollection( pClientProperties );
+        final ContextMenu polarResponseContextMenu = XActionUtilities
+                .createContextMenu( polarResponseContextMenuActionCollection );
+        return polarResponseContextMenu;
+    }
+
+    public static MenuBar getPolarResponseMenuBar( final ClientProperties pClientProperties,
+                                                   final PolarResponseActions polarResponseActions ) {
+        final Collection< Action > polarResponseMenuBarActionCollection = polarResponseActions
+                .getPolarResponseMenuBarActionCollection( pClientProperties );
+        final MenuBar polarResponseMenuBar = XActionUtilities
+                .createMenuBar( polarResponseMenuBarActionCollection );
+        return polarResponseMenuBar;
+    }
+
+    public static Menu getScaleMenu( final ClientProperties pClientProperties,
+                                     final PolarResponseAmplitudeScaleChoices polarResponseAmplitudeScaleChoices ) {
+        final XActionGroup scaleChoiceGroup = PolarResponseLabeledActionFactory
+                .getScaleChoiceGroup( pClientProperties, polarResponseAmplitudeScaleChoices );
+        final Menu scaleMenu = XActionUtilities.createMenu( scaleChoiceGroup );
+        return scaleMenu;
+    }
+
+    public static Menu getTestMenu( final ClientProperties pClientProperties,
+                                    final TestActions testActions ) {
+        final XActionGroup testActionGroup = PolarResponseLabeledActionFactory
+                .getTestActionGroup( pClientProperties, testActions );
+        final Menu testMenu = XActionUtilities.createMenu( testActionGroup );
+        return testMenu;
+    }
+
+    public static Menu getViewMenu( final ClientProperties pClientProperties,
+                                    final PolarResponseViewActions polarResponseViewActions ) {
+        final XActionGroup viewActionGroup = PolarResponseLabeledActionFactory
+                .getViewActionGroup( pClientProperties, polarResponseViewActions );
+        final Menu viewMenu = XActionUtilities.createMenu( viewActionGroup );
+        return viewMenu;
+    }
+}
+
